f92047ac94ea40f0025884049b12007c2b4acd51
1526210107 <1526210107@qq.com>
2019-11-12 09:06:36 +0800

    添加post请求参数处理

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
-const querystring = require('querystring');
-const handlerBlogRouter = require('./src/router/blog');
-const handlerUserRouter = require('./src/router/user');
-const serverHandler = (req, res) => {
-  res.setHeader("Content-Type", "application/json");
+const querystring = require('querystring')
+const handlerBlogRouter = require('./src/router/blog')
+const handlerUserRouter = require('./src/router/user')
 
-  const url = req.url;
-  req.path = url.split("?")[0];
-  req.query = querystring.parse(url.split("?")[1]);
-
-  // 处理博客请求
-  const blogData = handlerUserRouter(req, res);
-  if( blogData ){
-    res.end( JSON.stringify(blogData) );
-    return ;
-  }
-
-  // 处理博客请求
-  const userData = handlerBlogRouter(req, res);
-  if( userData ){
-    res.end( JSON.stringify(userData) );
-    return ;
-  }
-
-  // 未命中路由，返回404
-  res.writeHead(404, {'Content-Type': 'text/html'});
-  res.write("<h1>404 Not Found</h1><br/>");
-  res.end();
+// 处理POST请求传递的参数
+const formatPostData = req => {
+  return new Promise((resolve, reject) => {
+    let postData = ''
+    req.on('data', chunk => {
+      postData += chunk
+    })
+    req.on('end', () => {
+      if (!postData) {
+        resolve({})
+        return;
+      }
+      if (req.headers['content-type'] !== 'application/json') {
+        resolve({})
+        return
+      }
+      resolve(JSON.parse(postData))
+    })
+  })
 }
-module.exports = serverHandler;
+
+const serverHandler = (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+
+  const url = req.url
+  req.path = url.split('?')[0]
+  req.query = querystring.parse(url.split('?')[1])
+
+  formatPostData(req).then(postData => {
+    req.body = postData
+    // 处理博客请求
+    const blogData = handlerUserRouter(req, res)
+    if (blogData) {
+      res.end(JSON.stringify(blogData))
+      return
+    }
+
+    // 处理博客请求
+    const userData = handlerBlogRouter(req, res)
+    if (userData) {
+      res.end(JSON.stringify(userData))
+      return;
+    }
+
+    // 未命中路由，返回404
+    res.writeHead(404, { 'Content-Type': 'text/html' })
+    res.write('<h1>404 Not Found</h1><br/>')
+    res.end()
+  })
+}
+module.exports = serverHandler
